d93df0d90bea0a1420f17ed18744e5249d1c6f8f
Guillermo Perez <geperezrsud@gmail.com>
2025-07-21 21:45:31 -0300

    ajuste de tamanho no copy do projeto

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -8,7 +8,7 @@ return(
 
     <h3>©Desenvolvimento2025</h3>
     <p className="paragrafo-copy">Está App Faz Parte de um Projeto ADS por Guillermo Perez Ala Itupeva, não é endossado pelo
-     Igreja de Jesucristo do Santo dos Ultimos Dias.</p>
+     Igreja de Jesucristo do Santos dos Ultimos Dias.</p>
       
 </div>
 
