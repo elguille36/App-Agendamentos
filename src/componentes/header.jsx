e9a8c73a597d04e45cf91e9bd4d9f39583685149
Guillermo Perez <geperezrsud@gmail.com>
2025-07-27 17:28:20 -0300

    mudanças no link admini

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -11,7 +11,7 @@ return(
     <img className='logo'src='/img/nome da igreja.png' width={100}></img>
     <h1 className='title'><strong>Ala Itupeva</strong></h1>
     </span>
-  <span className='administrador'><Link className='link-adm' to='/admpage' element ={<AdminPage/>}>@Admin</Link></span>
+  <span className='administrador'><Link className='link-adm' to='/admpage' element ={<AdminPage/>}>Admin</Link></span>
 </header>
 </>
 )
