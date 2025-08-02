b0abc622eb468db42a385fd297b726da9d7c199e
Guillermo Perez <geperezrsud@gmail.com>
2025-08-01 21:25:37 -0300

    mudanças no imput tirando o placeholder

--- src/componentes/cadastroagendamento.jsx
+++ src/componentes/cadastroagendamento.jsx
@@ -64,7 +64,6 @@ const handleSubmit = async (e) => {
       <input
         type="date"
         name="data"
-        placeholder="Data"
         value={form.data}
         onChange={handleChange}
         className="w-full p-2 border border-gray-300 rounded bg-blue-100"
@@ -74,7 +73,6 @@ const handleSubmit = async (e) => {
       <input
         type="time"
         name="horario"
-        placeholder="Horario"
         value={form.horario}
         onChange={handleChange}
         className="w-full p-2 border border-gray-300 rounded bg-blue-100"
